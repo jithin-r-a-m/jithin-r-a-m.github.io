ec543845f894f2067a67a239d2bec7723623c692
Jithin R <94976118+jithin-r-a-m@users.noreply.github.com>
2024-03-22 08:17:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const menuToggle= document.querySelector(".menu-bars");
+const nav = document.querySelector("nav ul");
+
+
+menuToggle.addEventListener("click", () => {
+  nav.classList.toggle("slide");
+
+  });
